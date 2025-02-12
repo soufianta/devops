caa28e428d60d9f13107a657638a0d8e739b74ac
Soufian Ta <ta.soufian@outlook.be>
2025-02-12 22:46:45 +0100

    testing pipeline

--- app.py
+++ app.py
@@ -1,12 +1,2 @@
-import requests as re
-import json
-
-req = re.get("https://api.apis.guru/v2/list.json")
-
-try:
-  js = json.loads(req.text)
-  print(js)
-except json.JSONDecodeError as err:
-  print("invalid json data")
-finally:
-  print("it's ok...")
+for i in "Hello":
+  print(i)
